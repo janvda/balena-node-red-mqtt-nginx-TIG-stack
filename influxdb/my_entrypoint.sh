f17e272634f6fa57730a11d0364a7fedb67320d1
Jan VdA <jan.vandenaudenaerde@gmail.com>
2018-11-15 16:09:26 +0100

    mounting USB drive in influxdb container

--- influxdb/my_entrypoint.sh
+++ influxdb/my_entrypoint.sh
@@ -3,7 +3,8 @@
 mkdir -p /mnt/usbdrive
 mount /mnt/usbdrive
 
-# here below original entrypoint.sh (see https://github.com/influxdata/influxdata-docker/tree/35d89882b57f333d9518b4556e7b872ce970e620/influxdb/1.7)
+# here below copy paste of original entrypoint.sh. see : 
+#  https://github.com/influxdata/influxdata-docker/tree/35d89882b57f333d9518b4556e7b872ce970e620/influxdb/1.7
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
