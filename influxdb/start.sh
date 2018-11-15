1a02d85db676d351f9d30becc324d595a4debf11
Jan VdA <jan.vandenaudenaerde@gmail.com>
2018-11-15 15:55:57 +0100

    mounting USB drive in influxdb container

--- influxdb/start.sh
+++ influxdb/start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-mkdir -p /mnt/influxdb
-mount /mnt/influxdb
+# JVA 2018-11-15 I have added following instructions to mount usbdrive
+mkdir -p /mnt/usbdrive
+mount /mnt/usbdrive
+
+# here below original entrypoint.sh (see https://github.com/influxdata/influxdata-docker/tree/35d89882b57f333d9518b4556e7b872ce970e620/influxdb/1.7)
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- influxd "$@"
+fi
+
+if [ "$1" = 'influxd' ]; then
+	/init-influxdb.sh "${@:2}"
+fi
+
+exec "$@"
