2cc1422c3c95d7ef0b892bf05fa26f310aa92cd8
Jan VdA <jan.vandenaudenaerde@gmail.com>
2018-11-15 16:19:40 +0100

    writing influxdb data to usb drive

--- influxdb/my_entrypoint.sh
+++ influxdb/my_entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# JVA 2018-11-15 I have added following instructions to mount usbdrive
+# JVA 2018-11-15 I have added following instructions to mount usbdrive and create subfolder influxdb
 mkdir -p /mnt/usbdrive
 mount /mnt/usbdrive
+
+mkdir -p /mnt/usbdrive/influxdb
 
 # here below copy paste of original entrypoint.sh. see : 
 #  https://github.com/influxdata/influxdata-docker/tree/35d89882b57f333d9518b4556e7b872ce970e620/influxdb/1.7
