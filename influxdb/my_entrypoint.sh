e2a06d234ff6eebca8cebecdb636a12e5d810379
Jan VdA <jan.vandenaudenaerde@gmail.com>
2018-11-23 08:37:56 +0100

    changed influxdb mount location to /mnt/influxdb

--- influxdb/my_entrypoint.sh
+++ influxdb/my_entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # JVA 2018-11-15 I have added following instructions to mount usbdrive and create subfolder influxdb
-mkdir -p /mnt/usbdrive
-mount /mnt/usbdrive
-
-mkdir -p /mnt/usbdrive/influxdb
+mkdir -p /mnt/influxdb
+mount /mnt/influxdb
 
 # here below copy paste of original entrypoint.sh. see : 
 #  https://github.com/influxdata/influxdata-docker/tree/35d89882b57f333d9518b4556e7b872ce970e620/influxdb/1.7
